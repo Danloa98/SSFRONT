424ea20c658b0f6c51d30e9781cc439b051e5073
dannloa <3258306811401@ingenieria.usac.edu.gt>
2022-09-04 15:05:31 -0600

    Wenas

---
+++ Proyecto1/frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Editar from './Components/Editar';
+import Eliminar from './Components/Eliminar';
+import AddFriends from './Components/AddFriends';
+import VerArchivos from './Components/VerArchivos';
+
+function App() {
+  return (
+    <div className="App">
+      wenas dani
+      <VerArchivos></VerArchivos>
+    </div>
+  );
+}
+
+export default App;
---
+++ Proyecto1/frontend/src/Components/Eliminar.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef, useState } from 'react'
+import '../estilos.css'
+import Select from 'react-select';
+
+
+
+
+
+
+
+
+function Eliminar() {
+
+  let list = [{ label: "hola.png", value: 1 },{ label: "So2.pdf", value: 2 },{ label: "mmm.txt", value: 3 }];
+  const [nombre, setNombre] = useState([])
+  const [inputvalue, setValue] = useState('')
+  const [selectedValued, setSelectedValue] = useState(null) //selectedValued es el valor que se selecciona en el filtro
+  const handleInputChance = value => {
+    setValue(value)
+  }
+
+  const handleChange = value => {
+    setSelectedValue(value)
+    console.log(value)
+    alert('Se eliminara '+value.label)
+  }
+  useEffect(() => {
+    setNombre('ola dani')
+  }, [])
+
+  return (
+    <div class="modal-backdrop">
+    <div class="modal">
+      <header class="modal-header"><h2>ELIMINAR ARCHIVO</h2></header>
+      <section class="modal-body">
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-4'></div>
+            <div className='col-md-4'>
+              <Select options={list} value={selectedValued} OnInputChange={handleChange} onChange={handleChange} />
+            </div>
+            <div className='con-md-4'></div>
+          </div>
+        </div>
+        <div class="img-upload">
+          <div class="imgPreview">
+            <img />
+          </div>
+          <h4 class="h4-file">{nombre}</h4>
+        </div>
+        <hr />
+      </section>
+      <h4>Confirmar Contraseña</h4>
+      <input id="input-pass" type="password" v-model="pass" />
+      <footer class="modal-footer">
+        <button type="button" class="btn-green" >
+          Eliminar
+        </button>
+        <button type="button" class="btn-green" >Cerrar</button>
+      </footer>
+    </div>
+  </div>
+  )
+}
+
+export default Eliminar
